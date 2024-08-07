bc647dc8daa97223e4ac122609dc5df1ebc83845
maksimkins <maks.aleshkov.04@gmail.com>
2024-08-07 19:15:30 +0400

    Create UpdateToken Method

--- TasteTrailIdentity/src/TasteTrailIdentity.Infrastructure/Authentication/Services/IdentityAuthService .cs
+++ TasteTrailIdentity/src/TasteTrailIdentity.Infrastructure/Authentication/Services/IdentityAuthService .cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Authentication;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
@@ -106,9 +102,91 @@ public class IdentityAuthService : IIdentityAuthService
         };
     }
 
-    public Task<AccessToken> UpdateToken(Guid refresh, string jwt)
+    public async Task<AccessToken> UpdateToken(Guid refresh, string jwt)
     {
-        throw new NotImplementedException();
+        if(jwt is null) {
+            throw new InvalidCredentialException("jwt is null!");
+        }
+
+        if(jwt.StartsWith("Bearer ")) {
+            jwt = jwt.Substring("Bearer ".Length);
+        }
+
+        var handler = new JwtSecurityTokenHandler();
+        var tokenValidationResult = await handler.ValidateTokenAsync(
+            jwt,
+            new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = _jwtOptions.Issuer,
+
+                ValidateAudience = true,
+                ValidAudience = _jwtOptions.Audience,
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(_jwtOptions.KeyInBytes)
+            }
+        );
+
+        if(tokenValidationResult.IsValid == false) {
+            throw new InvalidCredentialException("invalid jwt token!");
+        }
+
+        var token = handler.ReadJwtToken(jwt);
+
+        Claim? idClaim = token.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier);
+
+        if(idClaim is null) {
+            throw new InvalidCredentialException($"Token has no claim with type '{ClaimTypes.NameIdentifier}'");
+        }
+
+        var userId = idClaim.Value;
+
+        var foundUser = await _userService.GetUserByIdAsync(userId);
+
+        if(foundUser is null) {
+            throw new InvalidCredentialException($"User not found by id: '{userId}'");
+        }
+
+        var oldRefreshToken = await _refreshTokenService.GetByIdAsync(refresh);
+
+        if(oldRefreshToken is null)
+        {
+            await _refreshTokenService.DeleteRangeRefreshTokensAsync(userId: userId);
+            throw new InvalidCredentialException("Refresh token not found!");
+        }
+
+        await _refreshTokenService.DeleteByIdAsync(refresh);
+
+        var newRefreshToken = await _refreshTokenService.CreateAsync(new RefreshToken{
+            UserId = userId
+        }) ;
+
+        var roles = await _userService.GetRoleByUsernameAsync(foundUser.UserName!);
+
+        var claims = roles
+            .Select(roleStr => new Claim(ClaimTypes.Role, roleStr))
+            .Append(new Claim(ClaimTypes.NameIdentifier, foundUser.Id.ToString()))
+            .Append(new Claim(ClaimTypes.Email, foundUser.Email ?? "not set"))
+            .Append(new Claim(ClaimTypes.Name, foundUser.UserName ?? "not set"));
+
+        var signingKey = new SymmetricSecurityKey(_jwtOptions.KeyInBytes);
+        var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+
+        var newToken = new JwtSecurityToken(
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(_jwtOptions.LifeTimeInMinutes),
+            signingCredentials: signingCredentials
+        );
+
+        var newTokenStr = handler.WriteToken(newToken);
+
+        return new AccessToken {
+            Refresh = newRefreshToken,
+            Jwt = newTokenStr,
+        };
     }
 
     public async Task<Guid> SignOutAsync(Guid refresh, string jwt)
