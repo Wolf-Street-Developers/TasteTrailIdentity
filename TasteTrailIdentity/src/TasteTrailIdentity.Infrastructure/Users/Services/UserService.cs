619338489cdbc1a9c021bb56d4cee14f3d9dab14
maksimkins <maks.aleshkov.04@gmail.com>
2024-08-07 13:00:27 +0400

    Add UserService

---
+++ TasteTrailIdentity/src/TasteTrailIdentity.Infrastructure/Users/Services/UserService.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TasteTrailData.Core.Roles.Enums;
+using TasteTrailData.Core.Users.Models;
+using TasteTrailData.Core.Users.Services;
+
+namespace TasteTrailIdentity.Infrastructure.Users.Services;
+
+public class UserService : IUserService
+{
+    private readonly UserManager<User> _userManager;
+
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public UserService(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public async Task<IdentityResult> CreateUserAsync(User user, string password)
+    {
+        return await _userManager.CreateAsync(user, password);
+    }
+    public async Task<IList<string>> GetRoleByUsernameAsync(string username)
+    {
+        var user  = await GetUserByUsernameAsync(username: username);
+        return await _userManager.GetRolesAsync(user);
+    }
+
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        return await _userManager.Users.ToListAsync();
+    }
+
+    public async Task<User> GetUserByIdAsync(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId) ?? throw new ArgumentException($"cannot find user with id: {userId}");
+
+        return user;
+    }
+
+
+    public async Task<User> GetUserByUsernameAsync(string username)
+    {
+        var user = await _userManager.FindByNameAsync(username) ?? throw new ArgumentException($"cannot find user with username: {username}");
+
+        return user;
+    }
+
+    public async Task<IdentityResult> UpdateUserAsync(User user)
+    {
+        return await _userManager.UpdateAsync(user);
+    }
+
+    public async Task<IdentityResult> DeleteUserAsync(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+
+        if (user == null)
+            return IdentityResult.Failed(new IdentityError { Description = $"User with ID {userId} not found." });
+
+        return await _userManager.DeleteAsync(user);
+    }
+
+    public async Task<IdentityResult> AssignRoleToUserAsync(string userId, UserRoles role)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        var roleName = role.ToString();
+
+        if (user == null)
+            return IdentityResult.Failed(new IdentityError { Description = $"User with id {userId} not found." });
+
+        var roles = await _userManager.GetRolesAsync(user);
+        var result = await _userManager.RemoveFromRolesAsync(user, roles);
+
+        if (!await _roleManager.RoleExistsAsync(roleName))
+            return IdentityResult.Failed(new IdentityError { Description = $"Role {roleName} not found." });
+
+        return await _userManager.AddToRoleAsync(user, roleName);
+    }
+
+    public async Task<IdentityResult> RemoveRoleFromUserAsync(string userId, UserRoles role)
+    {
+        var user = await _userManager.FindByNameAsync(userId);
+        var roleName = role.ToString();
+
+        if (user == null)
+            return IdentityResult.Failed(new IdentityError { Description = $"User with id {userId} not found." });
+
+        if (!await _roleManager.RoleExistsAsync(roleName))
+            return IdentityResult.Failed(new IdentityError { Description = $"Role {roleName} not found." });
+
+        return await _userManager.RemoveFromRoleAsync(user, roleName);
+    }
+
+    public async Task<bool> HasRegisteredUsers()
+    {
+        return await _userManager.Users.AnyAsync();
+    }
+
+    public async Task ToggleMuteUser(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+
+        if (user is null)
+            throw new Exception("User not found!");
+
+        user.IsMuted = !user.IsMuted;
+        await _userManager.UpdateAsync(user);
+
+        var claims = await _userManager.GetClaimsAsync(user);
+        var isMutedClaim = claims.FirstOrDefault(c => c.Type == "IsMuted");
+
+        if (isMutedClaim == null)
+            throw new Exception("Muted claim doesn't exist!");
+
+        await _userManager.ReplaceClaimAsync(user, isMutedClaim, new Claim("IsMuted", user.IsMuted.ToString()));
+    }
+
+    public async Task ToggleBanUser(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+
+        if (user is null)
+            throw new Exception("User not found!");
+
+        user.IsBanned = !user.IsBanned;
+        await _userManager.UpdateAsync(user);
+    }
+
+    public async Task<IList<Claim>> GetUserClaimsAsync(User user)
+    {
+        return await _userManager.GetClaimsAsync(user);
+    }
+
+    public async Task AddUserClaimAsync(User user, Claim claim)
+    {
+        var existingClaim = (await _userManager.GetClaimsAsync(user))
+                .FirstOrDefault(c => c.Type == claim.Type);
+
+        if (existingClaim is null)
+            await _userManager.AddClaimAsync(user, claim);
+    }
+}
