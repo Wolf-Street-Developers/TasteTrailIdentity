956188dc29a6f8e799d4f1e02859ad33827f9df6
maksimkins <maks.aleshkov.04@gmail.com>
2024-09-05 16:27:06 +0400

    Add GetUsersFilteredAsync to IAdminService

--- TasteTrailIdentityManager/src/TasteTrailIdentityManager.Core/Common/Admin/Services/IAdminService.cs
+++ TasteTrailIdentityManager/src/TasteTrailIdentityManager.Core/Common/Admin/Services/IAdminService.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using TasteTrailData.Core.Users.Models;
+using TasteTrailData.Infrastructure.Filters.Dtos;
 using TasteTrailIdentityManager.Core.Roles.Enums;
 
 namespace TasteTrailIdentityManager.Core.Common.Admin.Services;
@@ -8,7 +9,13 @@ public interface IAdminService
 {
     Task<IdentityResult> AssignRoleToUserAsync(string userId, UserRoles role);
 
-    Task<IEnumerable<User>> GetUsersAsync(int PageNumber, int PageSize);
+    Task<FilterResponseDto<User>> GetUsersFilteredAsync(PaginationParametersDto paginationParameters);
+
+    Task<FilterResponseDto<User>> GetUsersFilteredAsync(PaginationSearchParametersDto paginationParameters);
+
+    Task<FilterResponseDto<User>> GetUsersFilteredAsync(FilterParametersDto filterParameters);
+
+    Task<FilterResponseDto<User>> GetUsersFilteredAsync(FilterParametersSearchDto filterParameters);
 
     Task<User> GetUserByUsernameAsync(string username);
 
