9863f72599de8e259b06e87f623cf1070882e9fb
maksimkins <maks.aleshkov.04@gmail.com>
2024-09-10 04:24:48 +0400

    Delete Dtos

--- TasteTrailIdentity/src/TasteTrailIdentity.Core/Users/Dtos/UpdateUserDto.cs
+++
@@ -1,7 +0,0 @@
-namespace TasteTrailIdentity.Core.Users.Dtos;
-
-public class UpdateUserDto
-{
-    public required string Name { get; set; }
-    public required string Email { get; set; }
-}
--- TasteTrailIdentity/src/TasteTrailIdentity.Core/Users/Dtos/UserResponseDto.cs
+++
@@ -1,9 +0,0 @@
-using TasteTrailData.Core.Users.Models;
-
-namespace TasteTrailIdentity.Core.Users.Dtos;
-
-public class UserResponseDto
-{
-    public required User User { get; set; }
-    public required ICollection<string> Roles { get; set; }
-}
